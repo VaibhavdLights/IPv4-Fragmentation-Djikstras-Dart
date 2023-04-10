ecb3f96c22b3ac8af0be78994ee81bc58e4a92f8
Vaibhav Prajapati <vaibhavprajapati0101@gmail.com>
2023-04-10 19:11:16 +0530

    final commit

--- main.dart
+++ main.dart
@@ -1,129 +1,189 @@
 import 'dart:io';
+import 'dart:math';
+import 'dart:collection';
 
-class Graph {
-  final int numVertices;
-  late List<List<Edge>> adjacencyList;
+final int INT_MAX = 2147483647;
 
-  Graph(this.numVertices) {
-    adjacencyList = List.generate(numVertices, (index) => []);
+class PriorityQueue<T> {
+  List<T> _items = [];
+  Comparator<T> _comparator;
+
+  PriorityQueue(this._comparator);
+
+  void enqueue(T item) {
+    var i = 0;
+    while (i < _items.length && _comparator(_items[i], item) < 0) {
+      i++;
+    }
+    _items.insert(i, item);
   }
 
-  void addEdge(int source, int destination, int weight) {
-    Edge edge = Edge(destination, weight);
-    adjacencyList[source].add(edge);
+  T dequeue() {
+    if (_items.isEmpty) {
+      throw StateError("Priority queue is empty");
+    }
+    return _items.removeAt(0);
   }
-}
 
-class Edge {
-  late int destination;
-  late int weight;
-
-  Edge(this.destination, this.weight);
+  bool get isEmpty => _items.isEmpty;
 }
 
 class Fragmentation {
-  int mtu;
-  int payload;
-  int header = 20;
-
+  int mtu, payload, header = 20;
   Fragmentation(this.mtu, this.payload);
-
   int noOfPackets() {
     int n = ((payload - header) / (mtu - header)).ceil();
     return n;
   }
 }
 
-List<List<int>> createGraph() {
-  int numVertices = int.parse(stdin.readLineSync()!);
-  int numEdges = int.parse(stdin.readLineSync()!);
+class Pair<A, B> {
+  final A first;
+  final B second;
 
-  Graph graph = Graph(numVertices);
-
-  for (int i = 0; i < numEdges; i++) {
-    List<String> edgeInput = stdin.readLineSync()!.split(' ');
-    int source = int.parse(edgeInput[0]);
-    int destination = int.parse(edgeInput[1]);
-    int weight = int.parse(edgeInput[2]);
-
-    graph.addEdge(source, destination, weight);
-  }
-
-  return graph.adjacencyList.map((edges) => edges.map((edge) => edge.destination).toList()).toList();
+  Pair(this.first, this.second);
 }
 
-List<int> dijkstra(List<List<int>> adjacencyList, int source, int destination) {
-  int numVertices = adjacencyList.length;
-  List<int> distances = List.filled(numVertices, 2147483647);
-  List<bool> visited = List.filled(numVertices, false);
-
-  distances[source] = 0;
-
-  for (int i = 0; i < numVertices - 1; i++) {
-    int minDistance = 2147483647;
-    int minVertex = -1;
-
-    for (int j = 0; j < numVertices; j++) {
-      if (!visited[j] && distances[j] < minDistance) {
-        minDistance = distances[j];
-        minVertex = j;
-      }
-    }
-
-    visited[minVertex] = true;
-
-    if (minVertex == destination) {
+void dijkstra(List<List<int>> graph, int src, int dest) {
+  int n = graph.length;
+  List<int> dist = List.filled(n, INT_MAX);
+  List<int> parent = List.filled(n, -1);
+  List<bool> visited = List.filled(n, false);
+  dist[src] = 0;
+  PriorityQueue<Pair<int, int>> pq =
+      PriorityQueue<Pair<int, int>>((a, b) => a.first.compareTo(b.first));
+  pq.enqueue(Pair<int, int>(0, src));
+  while (!pq.isEmpty) {
+    int u = pq.dequeue().second;
+    if (u == dest) {
+      print("\nShortest Distance is ${dist[dest]}\n");
       break;
     }
-
-    for (int edge in adjacencyList[minVertex]) {
-      int newDistance = distances[minVertex] + 1;
-      if (newDistance < distances[edge]) {
-        distances[edge] = newDistance;
+    if (visited[u]) continue;
+    visited[u] = true;
+    for (int v = 0; v < n; v++) {
+      if (graph[u][v] == INT_MAX) continue;
+      int ans = dist[u] + graph[u][v];
+      if (ans < dist[v]) {
+        dist[v] = ans;
+        parent[v] = u;
+        pq.enqueue(Pair<int, int>(dist[v], v));
       }
     }
   }
+  List<int> path = [];
+  int u = dest;
+  while (u != -1) {
+    path.add(u);
+    u = parent[u];
+  }
+  path = path.reversed.toList();
+  for (int z = 0; z < path.length; z++) {
+    stdout.write("${path[z]} ");
+    if (z < path.length - 1) {
+      stdout.write("-> ");
+    }
+  }
+  print("");
+}
 
-  return distances;
+// List<List<int>> createGraph() {
+//   int n;
+//   stdout.write("Enter the number of Vertices ->> ");
+//   n = int.parse(stdin.readLineSync()!);
+//   List<List<int>> graph = List.generate(
+//       n, (i) => List.filled(n, 0, growable: false),
+//       growable: false);
+//   stdout.write("Enter the Adjacency Matrix of the graph ->> \n");
+//   for (int i = 0; i < n; i++) {
+//     List<int> row =
+//         stdin.readLineSync()!.split(" ").map(int.parse).toList(growable: false);
+//     for (int j = 0; j < n; j++) {
+//       graph[i][j] = row[j];
+//     }
+//   }
+//   stdout.write("Adjacency Matrix of the Graph ->> \n");
+//   for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < n; j++) {
+//       stdout.write("${graph[i][j]} ");
+//     }
+//     stdout.write("\n");
+//   }
+//   return graph;
+// }
+
+List<List<int>> createGraph() {
+  int n, e;
+  stdout.write("Enter the number of Vertices ->> ");
+  n = int.parse(stdin.readLineSync()!);
+
+  // Initialize adjacency matrix with all values set to infinity
+  List<List<int>> graph = List.generate(n, (_) => List.filled(n, INT_MAX));
+
+  // Set diagonal values to 0
+  for (int i = 0; i < n; i++) {
+    graph[i][i] = 0;
+  }
+
+  stdout.write("Enter the number of Edges ->> ");
+  e = int.parse(stdin.readLineSync()!);
+
+  // Read edges and update adjacency matrix
+  for (int i = 0; i < e; i++) {
+    int source, destination, weight;
+    stdout.write("Enter Source, Destination and Weight of Edge ${i + 1} ->> ");
+    List<String> input = stdin.readLineSync()!.split(" ");
+    source = int.parse(input[0]);
+    destination = int.parse(input[1]);
+    weight = int.parse(input[2]);
+    graph[source][destination] = weight;
+  }
+
+  stdout.write("Adjacency Matrix of the Graph ->> \n");
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (graph[i][j] == INT_MAX) {
+        stdout.write("$INT_MAX ");
+      } else {
+        stdout.write("${graph[i][j]} ");
+      }
+    }
+    stdout.write("\n");
+  }
+  return graph;
 }
 
 void main() {
   int mtu, payload;
-
-  stdout.write('Enter the value of MTU ->> ');
+  print("Enter the value of MTU ->> ");
   mtu = int.parse(stdin.readLineSync()!);
-
-  stdout.write('Enter the value of payload (including header size) ->> ');
+  print("Enter the value of payload(including header size) ->> ");
   payload = int.parse(stdin.readLineSync()!);
-
   Fragmentation fragments = Fragmentation(mtu, payload);
-  int packetSize = fragments.noOfPackets();
-  List<List<int>> graph = [];
-
-  for (int i = 0; i < packetSize; i++) {
-    int choice;
-    stdout.write('Do you want to create a new topology?\n1.Yes(1)\n2.No(0)\n->> ');
+  int packet_size = fragments.noOfPackets();
+  print("Packet Size ->> $packet_size\n");
+  List<List<int>> graph = createGraph();
+  int choice;
+  for (int j = 0; j < packet_size; j++) {
+    print("Do you to create a new topology? \n(0 ->>  NO\n1 ->> YES) \n->> ");
     choice = int.parse(stdin.readLineSync()!);
-
     if (choice == 1) {
-      List<List<int>> newGraph = createGraph();
+      List<List<int>> new_graph = createGraph();
       int src, dst;
-      stdout.write('Enter source for packet $i ->> ');
+      print("Enter source for packet $j ->> ");
       src = int.parse(stdin.readLineSync()!);
-      stdout.write('Enter destination for packet $i->> ');
+      print("Enter destination for packet $j ->> ");
       dst = int.parse(stdin.readLineSync()!);
-
-      print('For node $i the shortest path is ->> ');
-      dijkstra(newGraph, src, dst);
-      graph = newGraph;
+      print("\nFor node $j the shortest path is ->> ");
+      dijkstra(new_graph, src, dst);
+      graph = new_graph;
     } else {
       int src, dst;
-      stdout.write('Enter source for packet $i ->> ');
+      print("Enter source for packet $j ->> ");
       src = int.parse(stdin.readLineSync()!);
-      stdout.write('Enter destination for packet $i ->> ');
+      print("Enter destination for packet $j ->> ");
       dst = int.parse(stdin.readLineSync()!);
-
-      print('For node $i the shortest path is ->> ');
+      print("\nFor node $j the shortest path is ->> ");
       dijkstra(graph, src, dst);
     }
   }
